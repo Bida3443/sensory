25158281f17654c880427b59cf7417f4c54b7c67
Bida3443 <alexanderbamise@gmail.com>
2026-03-06 14:20:44 -0800

    sensory

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@
 
 import Image from "next/image";
 
-import React, {useState} from "react";
+
+import React, {useState} from "react"; 
 
 import Group from "../../sensory/public/Group.png";
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "./theme-toggle";
 
 const items = [
   {
@@ -12,7 +15,7 @@ const items = [
       <svg 
     xmlns="http://www.w3.org/2000/svg" 
     viewBox="0 0 384 512"
-    className="stroke-white fill-black w-7 h-7"><path d="M292.9 384c7.3-22.3 21.9-42.5 38.4-59.9 32.7-34.4 52.7-80.9 52.7-132.1 0-106-86-192-192-192S0 86 0 192c0 51.2 20 97.7 52.7 132.1 16.5 17.4 31.2 37.6 38.4 59.9l201.7 0zM288 432l-192 0 0 16c0 44.2 35.8 80 80 80l32 0c44.2 0 80-35.8 80-80l0-16zM184 112c-39.8 0-72 32.2-72 72 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-66.3 53.7-120 120-120 13.3 0 24 10.7 24 24s-10.7 24-24 24z"/></svg>
+    className="stroke-white fill-black dark:fill-white w-7 h-7"><path d="M292.9 384c7.3-22.3 21.9-42.5 38.4-59.9 32.7-34.4 52.7-80.9 52.7-132.1 0-106-86-192-192-192S0 86 0 192c0 51.2 20 97.7 52.7 132.1 16.5 17.4 31.2 37.6 38.4 59.9l201.7 0zM288 432l-192 0 0 16c0 44.2 35.8 80 80 80l32 0c44.2 0 80-35.8 80-80l0-16zM184 112c-39.8 0-72 32.2-72 72 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-66.3 53.7-120 120-120 13.3 0 24 10.7 24 24s-10.7 24-24 24z"/></svg>
     ),
     text: "Sensory",
     link: ""
@@ -52,19 +55,25 @@ export default function Home() {
   return (
     <section>
 
-      <nav className="mx-auto max-h-screen bg-white border-b border-black py-4 px-24">
-        <div className="flex text-black items-center justify-between">
+      <nav className="mx-auto max-h-screen bg-white dark:bg-black border-b transition-all duration-300 border-black dark:border-white py-4 px-24">
+        <div className="flex text-black dark:text-white transition-all duration-300 items-center justify-between">
           <div>
             {items.map((items,index)=>(
-              <div key={index} className="flex gap-2 items-center text-black">
+              <div key={index} className="flex gap-2 items-center dark:text-white text-black">
                 <div>{items.svg}</div>
+                
                 <div>{items.text}</div>
+                {/* <div>
+                  <Button>Button 1</Button>
+                  <Button variant="secondary">Button 2</Button>
+                </div> */}
+                <ThemeToggle/>
               </div> 
             ))}
           </div>
 
           <div className="hidden lg:block">
-        <button className="bg-black text-white px-6 py-3">
+        <button className="bg-black dark:bg-white text-white dark:text-black px-6 py-3">
         Contribute to this Service
         </button>
           </div>
@@ -77,7 +86,7 @@ export default function Home() {
             <svg 
               xmlns="http://www.w3.org/2000/svg" 
               viewBox="0 0 448 512"
-              className="h-7 w-7 stroke-black"
+              className="h-7 w-7 fill-black  dark:fill-white"
               >
               <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/>
               </svg>
@@ -90,23 +99,23 @@ export default function Home() {
 
       {/* Hero Section */}
 
-      <div className="bg-white pb-5 px-24  lg:h-screen  ">
+      <div className="bg-white dark:bg-black pb-5 px-24  lg:h-screen  ">
         
           <div className="flex lg:flex-row flex-col justify-between  items-center">
           <div className="flex flex-col order-2 lg:order-1 lg:justify-center lg:items-start items-center">
             <div className=" max-w-xl py-6 ">
-            <h1 className="text-black lg:text-8xl text-6xl lg:justify-start lg:text-start text-center justify-center font-semibold tracking-tight">Status:<span className="text-yellow-400">ON</span></h1>
-            <p className="text-black lg:text-justify max-w-2xl lg:justify-start text-center justify-center ">NB: The light status information provided on this website is intended for reference purposes only. While we strive to offer accurate and up-to-date data, we cannot guarantee its absolute precision.</p>
+            <h1 className="text-black dark:text-white lg:text-8xl text-6xl lg:justify-start lg:text-start text-center justify-center font-semibold tracking-tight">Status:<span className="text-yellow-400">ON</span></h1>
+            <p className="text-black dark:text-white lg:text-justify max-w-2xl lg:justify-start text-center justify-center ">NB: The light status information provided on this website is intended for reference purposes only. While we strive to offer accurate and up-to-date data, we cannot guarantee its absolute precision.</p>
           </div>
 
           <div className=" flex flex-col lg:text-start text-center  ">
-          <h1 className="underline text-2xl text-black ">Ligth History</h1>  
+          <h1 className="underline text-2xl text-black dark:text-white ">Ligth History</h1>  
           
           <div className="space-y-2">
             {sensory.map((sensory,index)=>(
               <div key={index}>
-                <div className="text-black font-bold">{sensory.texts}</div>
-                <div className="text-black text-sm">{sensory.date}</div>
+                <div className="text-black dark:text-white font-bold">{sensory.texts}</div>
+                <div className="text-black dark:text-white text-sm">{sensory.date}</div>
               </div>
             ))}
           </div>
@@ -127,7 +136,7 @@ export default function Home() {
         </div>
       
             {/* Footer Section */}
-      <div className="text-black gap-6 bg-white flex lg:flex-row flex-col justify-center lg:justify-between border-t items-center border-black py-6 px-24 ">
+      <div className="text-black dark:text-white gap-6 bg-white dark:bg-black flex lg:flex-row flex-col justify-center lg:justify-between border-t items-center border-black dark:border-white py-6 px-24 ">
         <p className="lg:order-1 order-2">© 2023 Sensory. All Rights Reserved.</p>
         <div className="flex lg:flex-row flex-col gap-6 items-center lg:order-2 order-1">
           <p>Privacy Policy</p>
