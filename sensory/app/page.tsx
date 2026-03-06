bfa89bf69f2b8375071c90ca507955f7252a4239
Bida3443 <alexanderbamise@gmail.com>
2026-03-06 05:53:51 -0800

    sensory

--- sensory/app/page.tsx
+++ sensory/app/page.tsx
@@ -62,6 +62,12 @@ export default function Home() {
             ))}
           </div>
 
+          <div className="hidden lg:block">
+        <button className="bg-black text-white px-6 py-3">
+        Contribute to this Service
+        </button>
+          </div>
+
           <div className="lg:hidden">
             <button onClick={toggleMenu} 
             className="bg-black text-white p-4 cursor-pointer "> Contribute to this Service
@@ -71,7 +77,6 @@ export default function Home() {
               className="h-7 w-7 stroke-black">
               <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
   
-
             </button>
           </div>
 
